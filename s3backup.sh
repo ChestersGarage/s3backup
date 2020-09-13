08f8d8ebf287115305cec18c992789d78efeb242
Mark Chester <mark@chesterfamily.org>
2020-09-14 00:41:38 +0100

    Code cleanup (syntax).

--- s3backup.sh
+++ s3backup.sh
@@ -10,21 +10,26 @@ AWSS3REGION=${AWSS3REGION:-us-east-1}
 LOCKFILE="/var/lock/s3backup.lock"
 LOGFILE="/var/log/s3backup.log"
 
-if [ ! -e $LOGFILE ]
+if [ ! -e ${LOGFILE} ]
 then
-  touch $LOGFILE
+  touch ${LOGFILE}
 fi
+
+# Write-to-log
+wtlog(){
+  echo "$(date) $1: $2" >> ${LOGFILE}
+}
 
 # Time stuff
 RUN_TIME=${RUN_TIME:-28800}
 
-if [[ "$PERIOD" ]] && [[ "$CRON_PATTERN" ]]
+if [[ "${PERIOD}" ]] && [[ "${CRON_PATTERN}" ]]
 then
-  echo "$(date) Both PERIOD and CRON_PATTERN were specified. Ignoring PERIOD." >> $LOGFILE
+  wtlog WARN "Both PERIOD and CRON_PATTERN were specified. Ignoring PERIOD."
 fi
 
 # Check for empty cron pattern
-if [[ -z "$CRON_PATTERN" ]]
+if [[ -z "${CRON_PATTERN}" ]]
 then
   # Set PERIOD to daily if not provided
   PERIOD=${PERIOD:-daily}
@@ -36,15 +41,15 @@ else
 fi
 
 # OK, let's go
-case $OPERATION in
+case ${OPERATION} in
   schedule)
-    if [[ -f $LOCKFILE ]]
+    if [[ -f ${LOCKFILE} ]]
     then
-      rm -f $LOCKFILE
+      rm -f ${LOCKFILE}
       killall aws
     fi
 
-    echo "$(date) Establishing AWS account settings." >> $LOGFILE
+    wtlog INFO "Establishing AWS account settings."
     mkdir -p /root/.aws
     cat > /root/.aws/config <<EOF
 [profile s3backup]
@@ -58,26 +63,24 @@ aws_secret_access_key = ${SECRET_ACCESS_KEY}
 EOF
     chmod -R go-rwx /root/.aws
 
-    if [[ $PERIOD ]]
+    if [[ ${PERIOD} ]]
     then
       # We drop all the info into an executable cron file under the necessary periodic cron folder
-      CRONFILE="/etc/periodic/$PERIOD/s3backup"
-      echo "$(date) The backup schedule is: $PERIOD." >> $LOGFILE
-      echo "$(date) Will back up the following data:" >> $LOGFILE
-      echo "" >> $LOGFILE
+      CRONFILE="/etc/periodic/${PERIOD}/s3backup"
+      wtlog INFO "The backup schedule is: ${PERIOD}."
+      wtlog INFO "Will back up the following data:"
       FOLDERS=$(ls --color=never /data)
-      for folder in ${FOLDERS}; do echo ${folder} >> ${LOGFILE}; done
-      echo "" >> $LOGFILE
-      echo "$(date) Writing cron file $CRONFILE." >> $LOGFILE
+      for folder in ${FOLDERS}; do wtlog INFO ${folder}; done
+      wtlog INFO "Writing cron file ${CRONFILE}."
       cat > $CRONFILE <<EOF
 #!/bin/sh
-ACCESS_KEY_ID=$ACCESS_KEY_ID
-SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY
-S3PATH="$S3PATH"
-AWSS3OPTIONS="$AWSS3OPTIONS"
+ACCESS_KEY_ID=${ACCESS_KEY_ID}
+SECRET_ACCESS_KEY=${SECRET_ACCESS_KEY}
+S3PATH="${S3PATH}"
+AWSS3OPTIONS="${AWSS3OPTIONS}"
 /bin/sh /s3backup.sh backup
 EOF
-      chmod +x $CRONFILE
+      chmod +x ${CRONFILE}
       cat > /etc/crontabs/root <<EOF
 # Make sure the periodic schedules are clean
 # min   hour    day     month   weekday command
@@ -90,59 +93,57 @@ EOF
     else
       # We append the info to root's crontab file, directly
       CRONFILE="/etc/crontabs/root"
-      echo "$(date) The backup schedule is: $CRON_PATTERN." >> $LOGFILE
-      echo "$(date) Will back up the following data:" >> $LOGFILE
-      echo "" >> $LOGFILE
+      wtlog INFO "The backup schedule is: ${CRON_PATTERN}."
+      wtlog INFO "Will back up the following data:"
       FOLDERS=$(ls --color=never /data)
-      for folder in ${FOLDERS}; do echo ${folder} >> ${LOGFILE}; done
-      echo "" >> $LOGFILE
+      for folder in ${FOLDERS}; do wtlog INFO ${folder}; done
       # Populate the cron file
-      echo "$(date) Writing schedule to cron file $CRONFILE." >> $LOGFILE
+      wtlog INFO "Writing schedule to cron file ${CRONFILE}."
       cat > $CRONFILE <<EOF
 #!/bin/sh
-ACCESS_KEY_ID=$ACCESS_KEY_ID
-SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY
-S3PATH="$S3PATH"
-AWSS3OPTIONS="$AWSS3OPTIONS"
-$CRON_PATTERN /bin/sh /s3backup.sh backup
+ACCESS_KEY_ID=${ACCESS_KEY_ID}
+SECRET_ACCESS_KEY=${SECRET_ACCESS_KEY}
+S3PATH="${S3PATH}"
+AWSS3OPTIONS="${AWSS3OPTIONS}"
+${CRON_PATTERN} /bin/sh /s3backup.sh backup
 EOF
     fi
 
     # Start the cron daemon
-    echo "$(date) Starting cron daemon." >> $LOGFILE
+    wtlog INFO "Starting cron daemon."
     crond
 
-    exec tail -Fn 25 $LOGFILE
+    exec tail -Fn 25 ${LOGFILE}
   ;;
   backup)
-    echo "$(date) Beginning backup..." | tee -a $LOGFILE
+    wtlog INFO "Beginning backup..."
     # Check for the lock file
-    if [ -e $LOCKFILE ]
+    if [ -e ${LOCKFILE} ]
     then
       # Finding a lock file ends this run
-      echo "$(date) Lock file $LOCKFILE detected. Skipping this backup run." | tee -a $LOGFILE
+      wtlog WARN "Lock file ${LOCKFILE} detected. Skipping this backup run."
       exit 0
     else
       # Grab start time and determine end time from run time
       START_TIME=$(date +%s)
-      END_TIME=$(( $START_TIME + $RUN_TIME ))
+      END_TIME=$(( ${START_TIME} + ${RUN_TIME} ))
 
       # Set the marker
-      touch $LOCKFILE
+      touch ${LOCKFILE}
 
       # Run this in the background so we can monitor and kill after run time elapses.
-      COMMAND="aws s3 sync --quiet /data/ $S3PATH $AWSS3OPTIONS --profile=s3backup"
-      echo "$(date) Running backup: \"$COMMAND\"" | tee -a $LOGFILE
-      $COMMAND 2>&1 | tee -a $LOGFILE &
+      COMMAND="aws s3 sync --quiet /data/ ${S3PATH} ${AWSS3OPTIONS} --profile=s3backup"
+      wtlog INFO "Running backup: \"${COMMAND}\""
+      ${COMMAND} 2>&1 | tee -a ${LOGFILE} &
 
       # Loop until we either time out or the command exits
       NOW=$(date +%s)
-      while [[ $NOW -le $END_TIME ]]
+      while [[ ${NOW} -le ${END_TIME} ]]
       do
         sleep 1
 	PROCS=$(ps axw | grep -v grep | grep aws | wc -l)
 	# If there aren't any running processes, we've finished
-	if [[ $PROCS -lt 1 ]]
+	if [[ ${PROCS} -lt 1 ]]
 	then
 	  break
 	fi
@@ -152,14 +153,14 @@ EOF
       # Either we timed out or finished
       # Kill it if it's still running.
       PROCS=$(ps axw | grep -v grep | grep aws | wc -l)
-      if [[ $PROCS -gt 0 ]]
+      if [[ ${PROCS} -gt 0 ]]
       then
         killall aws
       fi
 
       # Clean up
-      rm -f $LOCKFILE
-      echo "$(date) Backup finished." | tee -a $LOGFILE
+      rm -f ${LOCKFILE}
+      wtlog INFO "Backup finished."
     fi
     exit 0
   ;;
@@ -171,14 +172,14 @@ EOF
     echo "    show     - Displays usage and configurations"
     echo ""
     echo "Schedule: ${CRON_PATTERN}${PERIOD}"
-    echo "Run time: $RUN_TIME"
+    echo "Run time: ${RUN_TIME}"
     echo "Current time: $(date) (Cron pattern: $(date '+%M %H %d %m %u'))"
-    echo "Log file: $LOGFILE"
+    echo "Log file: ${LOGFILE}"
     echo "Data folders: $(ls /data)"
     exit 0
   ;;
   stop)
     killall aws
-    echo "$(date) Forced stop by command." | tee -a $LOGFILE
+    wtlog WARN "Forced stop by command."
 esac
 exit 0
